3709870330fe8ecf5b3a6701f462d7ea4334cb16
ShaiberAlon <alon.shaiber@gmail.com>
2020-08-11 15:01:52 -0400

    fix tests
    removed things from debug

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -6,9 +6,8 @@ import snakeski.workflows as w
 from snakeski.workflows import WorkflowSuperClass
 
 os.makedirs('sandbox/test-output', exist_ok = True)
-s = w.SnakefileGenerator(argparse.Namespace(config_file = 'sandbox/c.json', name = 't', output_dir = 'sandbox/test-output'))
 
 W = WorkflowSuperClass(argparse.Namespace(config_file='sandbox/config.json'))
 W.init('bla')
 
-#s = w.SnakefileGenerator(argparse.Namespace(config_file = 'sandbox/config.json', name = 't', output_dir = 'sandbox/test-output'))
+s = w.SnakefileGenerator(argparse.Namespace(config_file = 'sandbox/config.json', name = 't', output_dir = 'sandbox/test-output'))
